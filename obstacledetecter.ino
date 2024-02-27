5032c68bf990be52c48d99d491a765b60ca43e12
ApurvSrivasta <161207637+ApurvSrivasta@users.noreply.github.com>
2024-02-27 20:13:09 +0530

    Add files via upload

---
+++ obstacledetecter.ino
@@ -0,0 +1,27 @@
+int ir =2;
+int buzzer =3;
+void setup() {
+ Serial.begin(9600);
+ pinMode(se,INPUT);
+ pinMode(buz,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println("nothing detected");
+if (digitalRead(se)==LOW)
+{
+  Serial.println("obstacle detected");
+  tone(buz,1000,1000);
+  delay(500);
+   tone(buz,1500,1000);
+  delay(500);
+tone(buz,1000,1000);
+ 
+
+}
+else{
+  digitalWrite(buz,LOW);
+}
+}
